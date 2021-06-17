8ca74dcbf5bc02aeaef9638dd2a97c1de5f0c1d6
moses1206 <mose23@naver.com>
2021-06-17 23:22:12 +0900

    lastId

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Op } = require('sequelize');
 const { Post, User, Image, Comment } = require('../models');
 
 const router = express.Router();
@@ -8,7 +9,18 @@ const router = express.Router();
 // @access  Public
 router.get('/', async (req, res, next) => {
   try {
+    const where = {}; //초기로딩일때
+
+    // 쿼리스트링을 받는 방법 , 초기로딩이 아닐때
+    // 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+    if (parseInt(req.query.lastId, 10)) {
+      // 아이디가 lastId보다 작은수
+      where.id = { [Op.lt]: parseInt(req.query.lastId, 10) }; // 보다 작은수
+    }
+
     const posts = await Post.findAll({
+      // where 안에 lastId보다 작은거라는 의미가 있따.
+      where,
       //   10개만 가져와라
       limit: 10,
       //   생성날짜가 최신의 포스트가 맨위로, 반대는 ASC
