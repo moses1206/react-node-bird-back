f9f2f012b44cf743ec2d537c1cb87e7f36bffb85
moses1206 <mose23@naver.com>
2021-06-20 18:09:06 +0900

    dinamic routing2

--- app.js
+++ app.js
@@ -74,11 +74,13 @@ app.use(passport.session());
 const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
 const userRouter = require('./routes/user');
+const hashtagRouter = require('./routes/hashtag');
 
 // Use Router
 app.use('/post', postRouter);
 app.use('/posts', postsRouter);
 app.use('/user', userRouter);
+app.use('/hashtag', hashtagRouter);
 
 app.listen(3065, () => {
   console.log('Sever Start!');
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
+const { Op } = require('sequelize');
 
-const { User, Post } = require('../models');
+const { User, Post, Image, Comment } = require('../models');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares.js');
 
 const router = express.Router();
@@ -46,14 +47,11 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-// @route   GET    /user/:id
-// @desc    Get a User Info
-// @access  Private
-router.get('/:userId', async (req, res, next) => {
-  // GET /user
+router.get('/:id', async (req, res, next) => {
+  // GET /user/3
   try {
     const fullUserWithoutPassword = await User.findOne({
-      where: { id: req.params.userId },
+      where: { id: req.params.id },
       attributes: {
         exclude: ['password'],
       },
@@ -74,18 +72,83 @@ router.get('/:userId', async (req, res, next) => {
         },
       ],
     });
-
     if (fullUserWithoutPassword) {
-      // 시퀄라이즈에서 보내진 데이터는 Json 타입이 아니라서 변환해준다.
-      // 개인정보 예방을 위해 데이터를 최소화한다.
       const data = fullUserWithoutPassword.toJSON();
       data.Posts = data.Posts.length;
-      data.Followers = data.Followers.length;
       data.Followings = data.Followings.length;
+      data.Followers = data.Followers.length;
       res.status(200).json(data);
     } else {
-      res.status(404).json('존재하지 않는 아이디 입니다.!!');
+      res.status(404).json('존재하지 않는 사용자입니다.');
     }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// @route   GET    /user/2/posts
+// @desc    조회한 유저의 포스트 가져오기
+// @access  Private
+router.get('/:userId/posts', async (req, res, next) => {
+  try {
+    const where = { UserId: req.params.userId };
+    // 쿼리스트링을 받는 방법 , 초기로딩이 아닐때
+    if (parseInt(req.query.lastId, 10)) {
+      where.id = { [Op.lt]: parseInt(req.query.lastId, 10) };
+    } // 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+    const posts = await Post.findAll({
+      // where 안에 lastId보다 작은거라는 의미가 있따.
+      where,
+      //   10개만 가져와라
+      limit: 10,
+      //   생성날짜가 최신의 포스트가 맨위로, 반대는 ASC
+      order: [
+        ['createdAt', 'DESC'],
+        [Comment, 'createdAt', 'DESC'],
+      ],
+      include: [
+        //   작성자 정보를 추가
+        {
+          model: User,
+          //   비밀번호만 빼고 가져온다
+          attributes: ['id', 'nickname'],
+        },
+        {
+          model: Image,
+        },
+        {
+          // 코멘트 단 사람
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ['id', 'nickname'],
+            },
+          ],
+        },
+        {
+          model: User, // 좋아요 누른 사람
+          as: 'Likers',
+          attributes: ['id'],
+        },
+        {
+          model: Post,
+          as: 'Retweet',
+          // 리트윗 게시글의 작성자와 이미지를 가져온다.
+          include: [
+            {
+              model: User,
+              attributes: ['id', 'nickname'],
+            },
+            {
+              model: Image,
+            },
+          ],
+        },
+      ],
+    });
+    res.status(200).json(posts);
   } catch (error) {
     console.error(error);
     next(error);
